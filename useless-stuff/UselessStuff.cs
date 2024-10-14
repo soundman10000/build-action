3f81c3af66ca7b7bbac3e36c11085927b38bead2
Malley <jmalley@goempyrean.com>
2024-10-14 10:41:35 -0500

    some useless stuff to test things

---
+++ useless-stuff/UselessStuff.cs
@@ -0,0 +1,8 @@
+﻿// This is a useless repository for testing git-hub build actions.
+
+namespace UselessStuff;
+
+public static class UselessStuff
+{
+    public static string TurnStringIntoHello(this string input) => "Hello";
+}
